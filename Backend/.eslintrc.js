ad2a87e7b63353f7f1a71f9f484d09f8c64a2847
YASH FADADU <fadaduyash1994@gmail.com>
2025-10-15 12:24:49 +0530

    Backend init

---
+++ Backend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    '*.js',
+  ],
+};
